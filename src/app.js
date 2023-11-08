721a4605dd59265668d177f50a28196d6300eab0
Sara Parra <131387591+sarap13@users.noreply.github.com>
2023-11-08 19:33:01 +0000

    FEAT excuse generator

--- src/app.js
+++ src/app.js
@@ -2,10 +2,31 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  // Enlazamos la variable excuse con el ID del HTML
+  let excuseVariable = document.getElementById("excuse");
+
+  // Creamos los array con las diferentes palabras.
+  let who = ["The dog", "My grandma", "His turtle", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "the keys", "the car"];
+  let when = [
+    "before the class",
+    "right on time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
+
+  // Generamos palabras random de cada array
+  let randomWho = who[Math.floor(Math.random() * who.length)];
+  let randomAction = action[Math.floor(Math.random() * action.length)];
+  let randomWhat = what[Math.floor(Math.random() * what.length)];
+  let randomWhen = when[Math.floor(Math.random() * when.length)];
+
+  // Creamos una variable donde ir poniendo las excusas
+  let newExcuse = `${randomWho} ${randomAction} ${randomWhat} ${randomWhen}`;
+
+  // Conectamos el ID del HTML con la nueva variable donde irán las excusas.
+  excuseVariable.innerHTML = newExcuse;
 };
